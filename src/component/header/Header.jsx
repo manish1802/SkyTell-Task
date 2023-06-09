bf6888895b22d1a03064813ec160ca384bbd595a
manish1802 <mk8418954@gmail.com>
2023-06-09 20:28:21 +0530

    Added input type

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -2,11 +2,25 @@ import React, { useState } from "react";
 import "../header/Header.css";
 export default function Header() {
   const [open, setOpen] = useState(false);
-  const checkbox = [1, 2, 3, 4, 5];
+  const checkbox = [
+    "India",
+    "Nepal",
+    "Bhutan",
+    "Bangladesh",
+    "Sri Lanka",
+    "Pakistan",
+    "China",
+    "U.S.A",
+    "Russia",
+  ];
+  const [valu, setValue] = useState()
   const handleclick = () => {
     setOpen(!open);
   };
-  const [selectedCheckboxes, setSelectedCheckboxes] = useState([1, 2]);
+  const [selectedCheckboxes, setSelectedCheckboxes] = useState([
+    "India",
+    "Nepal",
+  ]);
 
   const handleCheckboxChange = (value) => {
     if (selectedCheckboxes.includes(value)) {
@@ -25,26 +39,41 @@ export default function Header() {
   return (
     <nav className="nav">
       <label className="dropdown" onClick={handleclick}>
-        Select Option
-        <img src="https://t3.ftcdn.net/jpg/04/83/39/12/360_F_483391217_rDo0Gdjp71zXabPRjeaE1O3I85R6nIgB.jpg" alt="" style={{width:13,marginLeft:20,}}/>
-
+        <span>{selectedCheckboxes.join(', ')} </span>
+        <img
+          src="https://t3.ftcdn.net/jpg/04/83/39/12/360_F_483391217_rDo0Gdjp71zXabPRjeaE1O3I85R6nIgB.jpg"
+          alt=""
+          style={{ width: 13, right: 0 }}
+        />
       </label>
       {open ? (
-        <div className="checkboxcontainer">
-          {checkbox.map((checkboxValue) => (
-            <label key={checkboxValue} className="inputfield">
-              <input
-                type="checkbox"
-                checked={selectedCheckboxes.includes(checkboxValue)}
-                onChange={() => handleCheckboxChange(checkboxValue)}
-                disabled={
-                  !selectedCheckboxes.includes(checkboxValue) &&
-                  selectedCheckboxes.length >= 2
-                }
-              />
-              Option  {checkboxValue}
-            </label>
-          ))}
+        <div className="maincontainer">
+          <div className="checkboxcontainer">
+            {checkbox.map((checkboxValue) => (
+              <div className="inputbox">
+                <label key={checkboxValue}>
+                  <input
+                    className="inputcheckbox"
+                    type="checkbox"
+                    checked={selectedCheckboxes.includes(checkboxValue)}
+                    onChange={() => handleCheckboxChange(checkboxValue)}
+                    disabled={
+                      !selectedCheckboxes.includes(checkboxValue) &&
+                      selectedCheckboxes.length >= 2
+                    }
+                  />
+                  <span
+                    style={{
+                      marginLeft: 10,
+                      fontFamily: "Arial, Helvetica, sans-serif",
+                    }}
+                  >
+                    {checkboxValue}
+                  </span>
+                </label>
+              </div>
+            ))}
+          </div>
         </div>
       ) : (
         ""
